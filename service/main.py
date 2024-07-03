1272cae66fc21621d7205ab1c6ec65bee3419afb
SecretDiscorder <139457966+SecretDiscorder@users.noreply.github.com>
2024-07-03 20:19:51 +0700

    1.0

---
+++ service/main.py
@@ -0,0 +1,28 @@
+import os
+from django.conf import settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from colors import add_markup
+from django.core.servers.basehttp import WSGIServer, WSGIRequestHandler, get_internal_wsgi_application
+
+
+logpath = os.getenv('PYTHON_SERVICE_ARGUMENT')
+
+
+class RequestHandler(WSGIRequestHandler):
+    def log_message(self, format, *args):
+        # Don't bother logging requests for admin images, or the favicon.
+        if (self.path.startswith(self.admin_static_prefix)
+                or self.path == '/favicon.ico'):
+            return
+
+        msg = "[%s] %s" % (self.log_date_time_string(), format % args)
+        kivymarkup = add_markup(msg, args)
+        with open(logpath, 'a') as fh:
+            fh.write(kivymarkup + '\n')
+            fh.flush()
+
+server_address = ('127.0.0.1', 8000)
+wsgi_handler = get_internal_wsgi_application()
+httpd = WSGIServer(server_address, RequestHandler)
+httpd.set_app(wsgi_handler)
+httpd.serve_forever()
